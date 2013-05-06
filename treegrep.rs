706d3c41f28c185c4291c26abea7069c89cba5c0
Grady O'Connell <flipcoder@flipcoder.com>
2013-05-05 23:57:00 -0700

    almost done (i think)

--- treegrep.rs
+++ treegrep.rs
@@ -1,11 +1,55 @@
 #!/usr/bin/rustc
+
+/*
+   treegrep
+   Like grep, but for trees, outlines, and when indentation matters
+ * Not yet finished!
+ */
+
+extern mod std;
+extern mod pcre;
 use core::os;
 use core::vec;
+use pcre::pcre::*;
+use pcre::consts::*;
+
+fn indent_level(line: &str) -> int {
+    0
+}
 
 fn grep(pattern: &str, filename: &str, in : @io::Reader) {
-    if in.eof() { return; }
+    let mut indent = -1;
+    let mut stack : ~[~str] = ~[];
 
-    io::println(fmt!("%s: %s", filename, in.read_line()));
+    while !in.eof() {
+        let line = in.read_line();
+
+        let cur_indent = indent_level(line);
+
+        match search(pattern, line, PCRE_CASELESS) {
+            Ok(_) => {
+                io::println(fmt!("%s: %s", filename, line));
+            }
+            Err(e) => match e {
+                CompileErr(_) => {
+                    io::println("error: bad pattern");
+                    return;
+                }
+                ExecErr(_) => {
+                }
+            }
+        }
+
+        // TODO: if cur => indent, push
+        if cur_indent >= indent {
+            stack += [line];
+        } else {
+            for stack.each |&blah| {
+                io::println(fmt!("%s: %s", filename, blah));
+            }
+        }
+        indent = cur_indent;
+    }
 }
 
 fn main() {
@@ -25,7 +69,7 @@ fn main() {
 
     let files = vec::slice(args, 2, args.len());
 
-    let multiple = files.len()>1;
+    /*let multiple = files.len()>1;*/
     for files.each |&d| {
         let p: ~Path = ~Path(d);
         let in = result::unwrap(io::file_reader(p));
